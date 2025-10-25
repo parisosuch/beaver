410974454d1f952576b976761d7bc503f7b4796f
Paris Osuch <paris.osuch@gmail.com>
2025-10-25 12:10:10 -0700

    Add note

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 
+// TODO: Will probably want to create a sqlite file for each project instead of storing all projects on the same db file
+
 const sqlite = new Database("mydb.sqlite");
 export const db = drizzle(sqlite);
