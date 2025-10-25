a043ac992ac0c5b3be05feb91d64f44360e358aa
Paris Osuch <paris.osuch@gmail.com>
2025-10-25 12:21:51 -0700

    create db using schema

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
+import * as schema from "./schema";
 
 // TODO: Will probably want to create a sqlite file for each project instead of storing all projects on the same db file
 
 const sqlite = new Database("mydb.sqlite");
-export const db = drizzle(sqlite);
+export const db = drizzle(sqlite, { schema });
