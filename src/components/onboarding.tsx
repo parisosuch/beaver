110c3d447cd0a4a232f0eb8afe0b085e97c81084
Paris Osuch <paris.osuch@gmail.com>
2025-10-26 12:51:12 -0700

    Create skeleton onboarding page

--- src/components/onboarding.tsx
+++ src/components/onboarding.tsx
@@ -1,17 +1,18 @@
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { Card } from "./ui/card";
 
 function Onboarding() {
   return (
-    <div className="w-full min-h-screen flex flex-row justify-center items-center">
-      <div className="w-1/2 h-full px-8 tracking-tighter">
+    <div className="w-full min-h-screen flex flex-row">
+      <div className="w-1/2 flex flex-col justify-center h-screen px-8 tracking-tighter bg-gray-100">
         <h1 className="text-7xl font-bold">Welcome</h1>
         <h1 className="text-7xl font-bold">To</h1>
         <h1 className="text-7xl font-bold">Beaver!</h1>
       </div>
-      <div className="w-1/2 h-full">
-        <div className="w-1/2">
+      <div className="w-1/2 flex justify-center items-center h-screen">
+        <Card className="w-1/2 p-4">
           <h2 className="text-3xl">Create your first project</h2>
           <div className="mt-4 space-y-2">
             <Label htmlFor="project-name">Project name</Label>
@@ -19,13 +20,13 @@ function Onboarding() {
               id="project-name"
               type="text"
               placeholder="beaver"
-              className="w-1/2"
+              className="w-full"
             />
           </div>
-          <div className="mt-4 w-full bg-blue-100">
+          <div className="mt-4 w-full flex justify-end">
             <Button>Create</Button>
           </div>
-        </div>
+        </Card>
       </div>
     </div>
   );
