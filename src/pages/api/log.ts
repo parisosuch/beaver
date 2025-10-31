c17348ebb6116267ccd0828d7676b5ed1d6afe2b
Paris Osuch <paris.osuch@gmail.com>
2025-10-30 17:21:20 -0700

    create api endpoint to post logs

--- src/pages/api/log.ts
+++ src/pages/api/log.ts
@@ -1,4 +1,4 @@
-import { getChannelLogs } from "@/lib/beaver/log";
+import { createLog, getChannelLogs } from "@/lib/beaver/log";
 import type { APIContext, APIRoute } from "astro";
 
 export type Log = {
@@ -27,6 +27,70 @@ export const GET: APIRoute = async ({ request }: APIContext) => {
     return new Response(JSON.stringify(logs), {
       status: 200,
       headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      return new Response(JSON.stringify({ error: err.message }), {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    console.error(err);
+    return new Response(
+      JSON.stringify({ error: "An unkown error has occurred." }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
+
+export const POST: APIRoute = async ({ request }: APIContext) => {
+  try {
+    // extract body and verify contents
+    const { message, level, channelId } = await request.json();
+    if (!message) {
+      return new Response(
+        JSON.stringify({ error: "message is a required field." }),
+        {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+    if (!level) {
+      return new Response(
+        JSON.stringify({ error: "level is a required field." }),
+        {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+    if (!channelId) {
+      return new Response(
+        JSON.stringify({ error: "channelId is a required field." }),
+        {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const log = await createLog({ message, level, channelId });
+
+    return new Response(JSON.stringify(log), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
   } catch (err) {
     if (err instanceof Error) {
