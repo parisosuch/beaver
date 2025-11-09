0c047e5db1955d38c8af571d76aacbe7fbc91296
Paris Osuch <paris.osuch@gmail.com>
2025-11-09 10:52:03 -0800

    ok

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,10 +1,18 @@
 import type { Event } from "@/lib/beaver/event";
-import { Card, CardTitle } from "./ui/card";
+import { Card, CardDescription, CardTitle } from "./ui/card";
 
 export default function EventCard({ event }: { event: Event }) {
   return (
-    <Card>
-      <CardTitle>{event.name}</CardTitle>
+    <Card className="p-8">
+      <CardTitle className="flex text-lg items-center space-x-4">
+        <div className="bg-gray-100 p-2 rounded-md">
+          {event.icon ? `${event.icon} ` : "🪵"}
+        </div>
+        <h2>{event.name}</h2>
+      </CardTitle>
+      {event.description ? (
+        <CardDescription>{event.description}</CardDescription>
+      ) : null}
     </Card>
   );
 }
