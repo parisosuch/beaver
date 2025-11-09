93a413f9d69685e9df25f6e13da43d170d6f6983
Paris Osuch <paris.osuch@gmail.com>
2025-11-09 10:55:27 -0800

    some more work

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,15 +1,18 @@
 import type { Event } from "@/lib/beaver/event";
-import { Card, CardDescription, CardTitle } from "./ui/card";
+import { Card, CardDescription } from "./ui/card";
 
 export default function EventCard({ event }: { event: Event }) {
   return (
     <Card className="p-8">
-      <CardTitle className="flex text-lg items-center space-x-4">
+      <div className="flex items-center space-x-4">
         <div className="bg-gray-100 p-2 rounded-md">
           {event.icon ? `${event.icon} ` : "🪵"}
         </div>
-        <h2>{event.name}</h2>
-      </CardTitle>
+        <div>
+          <h2 className="text-lg font-medium">{event.name}</h2>
+          <p className="text-sm font-light">#{event.channelId}</p>
+        </div>
+      </div>
       {event.description ? (
         <CardDescription>{event.description}</CardDescription>
       ) : null}
