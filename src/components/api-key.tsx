83027448f37e608c4b7a8108e20e309459b00d4a
Paris Osuch <paris.osuch@gmail.com>
2025-11-11 21:57:52 -0800

    this works for now

--- src/components/api-key.tsx
+++ src/components/api-key.tsx
@@ -1,21 +1,32 @@
 import type { Project } from "@/lib/beaver/project";
-import { Clipboard } from "lucide-react";
+import { Clipboard, ClipboardCheck } from "lucide-react";
+import { useState } from "react";
 
 export default function APIKey({ project }: { project: Project }) {
+  const [copied, setCopied] = useState(false);
+
   const handleCopy = () => {
     try {
+      setCopied(false);
       navigator.clipboard.writeText(project.apiKey);
+      setCopied(true);
     } catch (error) {
       console.error(error);
     }
   };
 
-  // TODO: add visual confirmation that the api key was copied to clipboard
   return (
     <div className="border px-1 rounded flex items-center justify-center">
       <p className="font-mono tracking-wider">{project.apiKey}</p>
       <button onClick={handleCopy} className="hover:cursor-pointer">
-        <Clipboard color="gray" className="bg-gray-200 rounded p-0.5 m-1" />
+        {copied ? (
+          <ClipboardCheck
+            color="gray"
+            className="bg-gray-200 rounded p-0.5 m-1"
+          />
+        ) : (
+          <Clipboard color="gray" className="bg-gray-200 rounded p-0.5 m-1" />
+        )}
       </button>
     </div>
   );
