13920b55bc7876bf4ee64681f7e84aed4e8a2aaf
Paris Osuch <paris.osuch@gmail.com>
2025-11-12 19:44:45 -0800

    update event card

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,21 +1,22 @@
 import type { Event, EventWithChannelName } from "@/lib/beaver/event";
 import { Card, CardDescription } from "./ui/card";
+import { getEventTime } from "@/lib/utils";
 
 export default function EventCard({ event }: { event: EventWithChannelName }) {
   return (
     <Card className="p-8">
       <div className="flex items-center space-x-4">
         <div className="bg-gray-100 p-2 rounded-md">
-          {event.icon ? `${event.icon} ` : "🪵"}
+          <p className="text-xl">{event.icon ? `${event.icon} ` : "🪵"}</p>
         </div>
         <div>
-          <h2 className="text-lg font-medium">{event.name}</h2>
-          <p className="text-sm font-light"># {event.name}</p>
+          <h2 className="text-xl font-medium">{event.name}</h2>
+          <div className="flex space-x-2 items-center text-sm text-black/75">
+            <p># {event.channelName}</p>
+            <p>{getEventTime(event.createdAt)}</p>
+          </div>
         </div>
       </div>
-      {event.description ? (
-        <CardDescription>{event.description}</CardDescription>
-      ) : null}
     </Card>
   );
 }
