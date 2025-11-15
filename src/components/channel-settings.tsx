9431660d30866ca7dc07e0684f9be5ea45f49dde
Paris Osuch <paris.osuch@gmail.com>
2025-11-15 10:14:50 -0800

    Handle channel delete event on settings page and change channel settings to contain all settings instead of a single one

--- src/components/channel-settings.tsx
+++ src/components/channel-settings.tsx
@@ -11,15 +11,16 @@ import {
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Trash2Icon } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Label } from "./ui/label";
 
-export default function ChannelSettings({ channel }: { channel: Channel }) {
+export default function ChannelSettings({ channels }: { channels: Channel[] }) {
+  const [clientChannels, setChannels] = useState<Channel[]>(channels);
   const [channelName, setChannelName] = useState("");
   const [channelDeleteError, setChannelDeleteError] = useState("");
   const [dialogOpen, setDialogOpen] = useState(false);
 
-  const deleteChannel = async () => {
+  const deleteChannel = async (channel: Channel) => {
     const res = await fetch("/api/channel", {
       method: "DELETE",
       body: JSON.stringify({ channelID: channel.id }),
@@ -33,56 +34,83 @@ export default function ChannelSettings({ channel }: { channel: Channel }) {
     }
   };
 
+  useEffect(() => {
+    const handleChannelDeleted = (e: CustomEvent<{ id: number }>) => {
+      const { id } = e.detail;
+
+      setChannels((prev) => prev.filter((c) => c.id !== id));
+    };
+
+    // add custom even for channel deletion
+    window.addEventListener(
+      "channel:deleted",
+      handleChannelDeleted as EventListener
+    );
+
+    return () => {
+      window.removeEventListener(
+        "channel:deleted",
+        handleChannelDeleted as EventListener
+      );
+    };
+  }, []);
+
   return (
-    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-      <div className="rounded border p-2 mt-4 flex justify-between items-center">
-        <div>
-          <h3 className="font-medium text-lg">{channel.name}</h3>
-          <p className="text-xs">{channel.createdAt?.toLocaleDateString()}</p>
-        </div>
-        <div>
-          <DialogTrigger asChild>
-            <Trash2Icon className="hover:cursor-pointer" />
-          </DialogTrigger>
-        </div>
-      </div>
-      {/* Create channel dialog */}
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Delete channel</DialogTitle>
-          <DialogDescription>
-            Are you sure you want to delete{" "}
-            <span className="font-bold"># {channel.name}</span> ?
-          </DialogDescription>
-        </DialogHeader>
-        <p className="font-medium text-sm text-rose-500">
-          {channelDeleteError}
-        </p>
-        <Label>Confirm channel name</Label>
-        <Input
-          id="channel-name"
-          placeholder="Channel name"
-          onChange={(e) => setChannelName(e.target.value)}
-        />
-        <div className="flex space-x-8 w-full justify-end mt-4">
-          <DialogClose asChild>
-            <Button variant="secondary">Cancel</Button>
-          </DialogClose>
-          <Button
-            disabled={channelName !== channel.name}
-            onClick={async () => {
-              deleteChannel();
-              const id = channel.id;
-              window.dispatchEvent(
-                new CustomEvent("channel:deleted", { detail: { id } })
-              );
-              setDialogOpen(false);
-            }}
-          >
-            Delete
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      {clientChannels.map((channel) => (
+        <Dialog open={dialogOpen} onOpenChange={setDialogOpen} key={channel.id}>
+          <div className="rounded border p-2 mt-4 flex justify-between items-center">
+            <div>
+              <h3 className="font-medium text-lg">{channel.name}</h3>
+              <p className="text-xs">
+                {channel.createdAt?.toLocaleDateString()}
+              </p>
+            </div>
+            <div>
+              <DialogTrigger asChild>
+                <Trash2Icon className="hover:cursor-pointer" />
+              </DialogTrigger>
+            </div>
+          </div>
+          {/* Delete channel dialog */}
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Delete channel</DialogTitle>
+              <DialogDescription>
+                Are you sure you want to delete{" "}
+                <span className="font-bold"># {channel.name}</span> ?
+              </DialogDescription>
+            </DialogHeader>
+            <p className="font-medium text-sm text-rose-500">
+              {channelDeleteError}
+            </p>
+            <Label>Confirm channel name</Label>
+            <Input
+              id="channel-name"
+              placeholder="Channel name"
+              onChange={(e) => setChannelName(e.target.value)}
+            />
+            <div className="flex space-x-8 w-full justify-end mt-4">
+              <DialogClose asChild>
+                <Button variant="secondary">Cancel</Button>
+              </DialogClose>
+              <Button
+                disabled={channelName !== channel.name}
+                onClick={async () => {
+                  deleteChannel(channel);
+                  const id = channel.id;
+                  window.dispatchEvent(
+                    new CustomEvent("channel:deleted", { detail: { id } })
+                  );
+                  setDialogOpen(false);
+                }}
+              >
+                Delete
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      ))}
+    </>
   );
 }
