202cb83d8f0fc2d3e35f5e4bf84ded99cfe563fe
Paris Osuch <paris.osuch@gmail.com>
2025-12-24 11:18:04 -0800

    create schema for user

--- src/lib/db/init.ts
+++ src/lib/db/init.ts
@@ -47,3 +47,12 @@ await db.run(`
     UNIQUE(event_id, key)
   )
 `);
+
+await db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at INTEGER NOT NULL
+  )
+`);
