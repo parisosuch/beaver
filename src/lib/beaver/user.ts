cbecf7c1db1a70f76030c2eaebcf780c7e054803
Paris Osuch <paris.osuch@gmail.com>
2025-12-24 12:15:07 -0800

    Create lib function to create user

---
+++ src/lib/beaver/user.ts
@@ -0,0 +1,37 @@
+import { db } from "../db/db";
+import { users } from "../db/schema";
+import { eq } from "drizzle-orm";
+
+export type DatabaseUser = {
+  id: number;
+  userName: string;
+  isAdmin: boolean;
+  password: string;
+  createdAt: Date | null;
+};
+
+export type User = {
+  id: number;
+  userName: string;
+  isAdmin: boolean;
+  createdAt: Date | null;
+};
+
+export async function createUser(
+  userName: string,
+  password: string,
+  isAdmin: boolean
+): Promise<User> {
+  const hashedPassword = await Bun.password.hash(password);
+
+  const res = await db
+    .insert(users)
+    .values({ userName, password: hashedPassword, isAdmin })
+    .returning();
+
+  const user: User = res[0];
+
+  delete (user as any).password;
+
+  return user;
+}
