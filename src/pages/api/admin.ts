76fe01d1318bb1f2e2f5fd54274979b7d4ef2a29
Paris Osuch <paris.osuch@gmail.com>
2025-12-24 12:21:59 -0800

    Create api route to create admin user

---
+++ src/pages/api/admin.ts
@@ -0,0 +1,45 @@
+import type { APIRoute } from "astro";
+import { createUser, getAdminUsers } from "@/lib/beaver/user";
+
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const { userName, password } = await request.json();
+
+    // check if an admin user already exists
+    const adminUsers = await getAdminUsers();
+
+    if (adminUsers.length > 0) {
+      return new Response(
+        JSON.stringify({ error: "An admin user already exists." }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    const user = await createUser(userName, password, true);
+
+    return new Response(JSON.stringify(user), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      return new Response(JSON.stringify({ error: err.message }), {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    console.error(err);
+    return new Response(
+      JSON.stringify({ error: "An unkown error has occurred." }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
+
+export const prerender = false;
