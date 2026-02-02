dac868ab08a6c7e7c547f64c3c14f5fba4528221
Paris Osuch <paris.osuch@gmail.com>
2026-02-01 19:26:04 -0800

    maint: add drizzle-kit configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "sqlite",
+  schema: "./src/lib/db/schema.ts",
+  dbCredentials: {
+    url: "beaver.sqlite",
+  },
+});
